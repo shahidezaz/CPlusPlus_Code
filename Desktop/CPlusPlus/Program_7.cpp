0df7fa041b7164a5132c077b1be2f1435a1c0004
shahid khan <shahidezaz@gmail.com>
2026-07-01 00:43:30 -0500

    1

---
+++ Desktop/CPlusPlus/Program_7.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char str1[]  = "shahid--";
+    printf("%s\n", str1);
+    int len;
+    len=strlen(str1);
+    printf("%d\n", len);
+    
+return 0;
+}
+
